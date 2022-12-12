6c0524aa6a038b08bdac4ffe0b256acb61c6d707
AnthonySaReis <anthonysareis11@gmail.com>
2022-12-12 11:40:04 -0300

    blank

--- pages/index.js
+++ pages/index.js
@@ -41,6 +41,8 @@ export default function Home() {
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
                   href="https://drive.google.com/file/d/1Ui2fQxsI46R3d4ljV95_fYyRPhi1Ssvh/view?usp=share_link"
+                  target="_blank"
+                  rel="noreferrer"
                 >
                   Curriculum
                 </a>
